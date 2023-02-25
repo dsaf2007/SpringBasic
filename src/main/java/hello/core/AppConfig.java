4070f85b85aca0df5ab1e98a2f2b34c4e6cb9181
dsaf2007 <dsaf2007@naver.com>
2023-02-26 01:24:25 +0900

    refactor : refactor AppConfig to remove duplicate

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemeberRepository;
@@ -8,12 +10,20 @@ import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
-
+ 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemeberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemeberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemeberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discoundPolicy());
+    }
+
+    public DiscountPolicy discoundPolicy() {
+        return new FixDiscountPolicy();
     }
 }

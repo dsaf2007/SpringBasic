e3b5fe397c7c9a5bba0b5d7dbd4a30f08b28ee33
dsaf2007 <dsaf2007@naver.com>
2023-03-05 22:37:06 +0900

    test : search beans with same types

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,12 +1,11 @@
 package hello.core;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
-import hello.core.member.MemoryMemeberRepository;
+import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 import org.springframework.context.annotation.Bean;
@@ -23,7 +22,7 @@ public class AppConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemeberRepository();
+        return new MemoryMemberRepository();
     }
 
     @Bean
